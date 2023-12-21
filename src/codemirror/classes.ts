9ec4b57969a76a05ff350f4ea393ca47effc6764
Guy Cohen <guyc1800@gmail.com>
2023-12-21 13:26:13 +0200

    a

--- src/codemirror/classes.ts
+++ src/codemirror/classes.ts
@@ -1,20 +1,33 @@
 import { Editor } from "codemirror";
-import { ExtendedEditor,Completion } from "./types";
+import { ExtendedEditor,Completion ,Hint} from "./types";
 import { Position } from "codemirror";
 import CodeMirror = require("codemirror");
-
+let LatexHints: Hint[]=[
+    {
+        text:'a',
+        displayText:'\\a',
+        description:"an a",
+        inline:false,
+    },
+    {
+        text:'b',
+        displayText:'\\b',
+        description:"an b",
+        inline:false,
+    }
+]
 export default class Autocomplete{
     constructor(private readonly context,private readonly editor: ExtendedEditor & Editor, private readonly cm:typeof CodeMirror){
         this.editor.on('cursorActivity', this.triggerHints.bind(this));
     }    
     private readonly doc=this.editor.getDoc();
-    private symbolRange?: {from:Poisition, to:Position};
+    private symbolRange?: {from:Position, to:Position};
     private readonly trigger_symbol = '\\';
     triggerHints(){
         const pos = this.doc.getCursor();
         const symbolRange = [{ line: pos.line, ch:pos.ch-this.trigger_symbol.length},pos] as const;
         const chars = this.doc.getRange(...symbolRange);
-        // TODO : check if the cursor is inside latex block 
+        // TODO  : check if the cursor is inside latex block 
         if(chars=== this.trigger_symbol && !/\S/.test(this.doc.getRange(pos,{line:pos.line, ch:pos.line+1}))){ 
             //second statment is to check that there is no char other then space after the cursor pos
             this.editor.showHint({
@@ -32,7 +45,7 @@ export default class Autocomplete{
         const {line, ch} = this.symbolRange.to; // the pos where trigger symbol ends
         const cursor = this.doc.getCursor();
         if(cursor.line < line || cursor.ch < ch){
-            return;
+            return;// return if the cursor is before the trigger symbol
         }
         const keyword = this.doc.getRange({line,ch},{line: cursor.line, ch:cursor.ch});// the chars after the triggersymbol of the completion
         const {from: completionFrom} = this.symbolRange;
@@ -42,13 +55,18 @@ export default class Autocomplete{
             to: CompletionTo,
             list:this.getHints(
                 keyword,
-                this.doc.getRange
-            )
-
-            }
-
-            /
-
+                this.doc.getRange({line:cursor.line, ch:0},{line:cursor.line, ch:cursor.ch-1}) //FIXME check if it fits the code
+            ),
+        };
+        return completion;
+    }
+    private getHints(keyword:string, indent:string): Hint[]{
+        let hints = [];
+        let TextIndent = indent.trimStart();
+        for(let hint of LatexHints){
+            if(hint.) // TODO sort matchs by history if no text after the trigger symbol, if there is sort by first "startwith" ,includes 
         }
+
+        
     }
 }
--- src/codemirror/types.ts
+++ src/codemirror/types.ts
@@ -1,18 +1,17 @@
 import { Position,Editor } from "codemirror";
 export interface Hint {
-    text: string;
-    displayText?: string;
-    className?: string;
-    description?: string;
+    text: string;//the output after completion
+    displayText?: string; // the keyword short
+    className?: string; 
+    description?: string;// description
     render?: (container: Element, completion: Completion, hint: Hint) => void;
-    // hint?: (cm: typeof CodeMirror, completion: Completion, hint: Hint) => void;
+    //TODO hint?: (cm: typeof CodeMirror, completion: Completion, hint: Hint) => void;
     inline: boolean;
-    //container:Element|null custom container for the widget
+    //TODO container:Element|null custom container for the widget
 //     select (completion, Element)
 // Fired when a completion is selected. Passed the completion value (string or object) and the DOM node that represents it in the menu.
 
 }
-
 export interface Completion {
     from: Position;
     to: Position;
